1b9471bb7ab28e621031e335632032622308c84b
Aron Ilie <aronilie.code@gmail.com>
2022-10-17 18:07:11 +0200

    Created Form component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,7 @@
 import ButtonStyled from "./ButtonStyled";
 
 const Button = () => {
-  return <ButtonStyled type="submit">GENERATE PASSWORD</ButtonStyled>;
+  return <ButtonStyled type="button">GENERATE PASSWORD</ButtonStyled>;
 };
 
 export default Button;
