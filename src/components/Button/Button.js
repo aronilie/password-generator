c9d8c21b87cb6701d825f6a7135733ed54ec78fe
Aron Ilie <aronilie.code@gmail.com>
2022-10-17 11:48:01 +0200

    Created Button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,7 @@
+import ButtonStyled from "./ButtonStyled";
+
+const Button = () => {
+  return <ButtonStyled type="submit">GENERATE PASSWORD</ButtonStyled>;
+};
+
+export default Button;
---
+++ src/components/Button/ButtonStyled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const ButtonStyled = styled.button`
+  background-color: blueviolet;
+  color: #fff;
+  border: none;
+  font-size: large;
+  font-weight: 700;
+  border-radius: 0.5em;
+  height: 70px;
+  width: 350px;
+`;
+
+export default ButtonStyled;
